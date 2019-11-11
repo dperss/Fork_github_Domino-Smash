462a64a91ae6338962b271cd2a7f1c3c9a30237d
Diogo Ramos <diogopaulo@protonmail.com>
2019-11-11 15:02:21 +0000

    Changeport mudada para de 0 a 20

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -10,8 +10,8 @@ public:
 	GLfloat xx, xy, yy, yx;
 
 	Caixadojogo() {
-		xx = 0;
-		yy = 1.99;
+		xx = 1;
+		yy = 19;
 	}
 	void DrawCaixa() {
 		glColor3f(0.0, 1.0, 0.0);
@@ -35,6 +35,9 @@ class Domino
 	GLint R = 0, G = 1, B = 0;
 	GLint hit = 0;
 	
+	Domino() {
+
+	}
 
 	Domino(GLfloat x, GLfloat y) {
 		this->x = x;
@@ -46,18 +49,18 @@ class Domino
 			glColor3f(R, G, B);
 			glBegin(GL_QUADS);
 			glVertex2f(x, y);
-			glVertex2f(x + 0.2, y);
-			glVertex2f(x + 0.2, y + 0.3);
-			glVertex2f(x, y + 0.3);
+			glVertex2f(x + 1, y);
+			glVertex2f(x + 1, y + 1.4);
+			glVertex2f(x, y + 1.4);
 			glEnd();
 				
 	}
 
 	void Domino_Compare(GLfloat cx, GLfloat cy) {
 		
-		if ( (cx>=x && cx<=x+0.2) && (cy>=y && cy<=y+0.3)) {//Compara se a bola bate no domino, ou se o domino bate no domino
+		if ( (cx>=x && cx<=x+1) && (cy>=y && cy<=y+1.4)) {//Compara se a bola bate no domino, ou se o domino bate no domino
 			
-			y += 0.4;
+			y += 2;
 			R = 1;
 			G = 0;			
 		}
@@ -67,16 +70,23 @@ class Domino
 
 };
 
-Domino domino(0.1,0.1);
-Domino a(0.1, 0.5);
+Domino a[5] =
+{
+Domino(5,4),
+Domino(5, 5.5),
+Domino(5, 7),
+Domino(5, 8.5 )
+};
+
 
 class Circulo {
 
 public:
-	GLfloat cx = 0, cy = 0, r = 0.08;
+	GLfloat cx = 10, cy = 2, r = 0.5;
 	GLint num_segments=124;
 
 	/*
+
 	http://slabode.exofire.net/circle_draw.shtml
 	*/
 	void DrawCircle()
@@ -128,39 +138,98 @@ class Shoot {
 	GLfloat x, y;
 	GLfloat directionx = 1;
 	GLfloat directiony = 1;
+	GLfloat Veloctity = 3;
 
 	void Mira()// dispara a bola
 	{
-		x = 0 + 0.5 * cos(angulo);
-		y = 0 + 0.5 * sin(angulo);
+		x = 10 + 2 * cos(angulo);
+		y = 2  + 2 * sin(angulo);
 
-		glLineWidth(1.5);
+		glLineWidth(3);
 		glColor3f(0.0, 0.4, 0.2);
 		glPointSize(3.0);
 		glBegin(GL_LINES);
-		glVertex2f(0, 0);
+		glVertex2f(10, 2);
 		glVertex2f(x, y);
 		glEnd();
 		if (shoot == 1) {
 
-			bola.cx += cos(angulo) / 750 * directionx;
-			bola.cy += sin(angulo) / 750 * directiony;
+			bola.cx += cos(angulo) / 750 * directionx* Veloctity;
+			bola.cy += sin(angulo) / 750 * directiony* Veloctity;
 		}
 	}
 	void Hit_box(GLfloat x, GLfloat y) {//deteta o limite do campo de jogo e muda a direçao da bola 
-		if (x >= 1.99 || x <= -1.99) {
-			directionx = -directionx;
+		if (x >= 19 || x <= 1) {
+			directionx = -directionx;//inverte a direçao no eixo do x
 		}
-		if (y >= 1.99 || y <= -1.99) {
-			directiony = -directiony;
+		if (y >= 19 || y <= 1) {
+			directiony = -directiony;//inverte a direçao no eixo do y
 		}
+	}
+	void Hit_box_domino(GLfloat x, GLfloat y,GLfloat cx, GLfloat cy) {//deteta quando a bola bate numa domino 
 
+		if ( cx>=x && cx<=x+1 && cy>=y && cy<=y+1.4) {
+			directionx = -directionx/2;
+			directiony = -directiony/2;
+	
+		}
+	}
+	void Hit_box_Caixa_obstaculo(GLfloat x, GLfloat y, GLfloat cx, GLfloat cy) {//deteta quando a bola bate numa Caixa_obstaculo 
+
+		if (cx >= x && cx <= x + 1.4 && cy >= y && cy <= y + 0.8) {//Umas vezes funciona outras não investigar ???
+			directionx = -directionx ;
+			directiony = -directiony ;
+			Veloctity *= 2;
+		}
+		
 	}
 
 
 };
 
 Shoot mira;
+
+class Caixa_obstaculo {
+public:
+	GLfloat x;
+	GLfloat y;
+	GLint R = 0, G = 1, B = 0;
+	GLint hit = 1;
+	GLfloat directionx = 1, directiony = 1;
+	GLfloat Velocity = 4;
+
+	Caixa_obstaculo() {
+
+	}
+	Caixa_obstaculo(GLfloat x, GLfloat y) {
+		this->x = x;
+		this->y = y;
+	}
+
+	void Drawcaixa_obstaculo() {
+
+		glColor3f(R, G, B);
+		glBegin(GL_QUADS);
+		glVertex2f(x, y);
+		glVertex2f(x + 1.4, y);
+		glVertex2f(x + 1.4, y + 0.8);
+		glVertex2f(x, y + 0.8);
+		glEnd();
+		x += cos(90) / 750 * directionx* Velocity;
+		//y += sin(0) / 750 * directiony;
+		if (x >= 19 || x <= 1) {
+			directionx = -directionx;//inverte a direçao no eixo do x
+		}
+		/*if (y >= 1.99 || y <= -1.99) {
+			directiony = -directiony;//inverte a direçao no eixo do y
+		}*/
+	}
+	
+		
+
+};
+
+Caixa_obstaculo box(10,3);
 
 
 
@@ -171,30 +240,35 @@ void display(void)
 	
 	glClearColor(0.0, 0.0, 0.0, 0.0); // define a cor do fundo da janela (RGBA)
 	glClear(GL_COLOR_BUFFER_BIT); // Aplica a cor atual no fundo
+	
 	glLoadIdentity();
 
 	glColor3f(0, 100, 0);
 	
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluOrtho2D(-2, 2, -2, 2);
+	gluOrtho2D(0, 20, 0, 20);
 	
 	caixa.DrawCaixa();
-	
-	mira.Mira();
+	mira.Hit_box_Caixa_obstaculo(box.x, box.y, bola.cx, bola.cy);	//AS hitboxes tenhem que aparecer sempre depois do draw senão da erros
 	mira.Hit_box(bola.cx, bola.cy);
+	mira.Mira();
 
-	/*domino.DrawDomino();
-	domino.Domino_Compare(bola.cx, bola.cy);
-	a.DrawDomino();
-	a.Domino_Compare(bola.cx, bola.cy);
-	a.Domino_Compare(domino.x, domino.y);
-	*/
+	for (int i = 0; i < 4; i++) {
+		mira.Hit_box_domino(a[i].x, a[i].y, bola.cx, bola.cy);
+		a[i].DrawDomino();
+		a[i].Domino_Compare(a[i-1].x, a[i - 1].y);
+		a[i].Domino_Compare(bola.cx, bola.cy);		
+	}
+	
+	box.Drawcaixa_obstaculo();
+	
 
 	bola.DrawCircle();
 	
+	
 
-
+	
 	glutPostRedisplay();
 	glFlush(); //despeja imgs da memoria para o ecran
 }
@@ -233,26 +307,27 @@ void ChangeSize(GLsizei w, GLsizei h)
 
 	// Set the clipping volume
 	glOrtho(0.0f, windowWidth, 0.0f, windowHeight, 1.0f, -1.0f);
+	//gluOrtho2D(-10, 10, -10, 10);
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 }
-
-
-
 
  void HandleKeyboard(unsigned char key, int x, int y) {
 
 	switch (key)
 	{
 	
-	case 45://space bar
+	case 32://space bar
 		mira.shoot = 1;
 		break;
-	
+	case 27://fechar o jogo
+		exit(1);
+		break;
 	}
 	glutPostRedisplay();
 }
+
  /*
 void HandleMouse(int button, int state, int x, int y) {
 	if (button == GLUT_LEFT_BUTTON)
@@ -288,7 +363,6 @@ void SpecialKeys(int key, int x, int y) {
 	glutPostRedisplay();
 }
 
-
 void main(int argc, char** argv)
 {
 	glutInit(&argc, argv); 
@@ -303,21 +377,3 @@ void main(int argc, char** argv)
 	SetupRC();
 	glutMainLoop();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
